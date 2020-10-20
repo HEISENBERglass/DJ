0230150631fe85a80e0786131aba16ac6eb979c6
HEISENBERglass <ZAAMG14@gmail.com>
2020-10-20 09:23:15 -0800

    h

--- tezbazar/website/views.py
+++ tezbazar/website/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 marsoxod2 = "bartsucks"
 def home(reguest):
 		dormamu = "asdasdadsasdasdasdasdasdasdasdadas"
-		return render(reguest , 'home.html' , {'zaporoj' = dormamu})
+		return render(reguest , 'home.html' , {'zaporoj' : dormamu})
 
 def blog(reguest1):
 	return render(reguest1, 'blog.html', {})
