060396ee1f094a1faf75724ee31adb99540c4d3e
HEISENBERglass <ZAAMG14@gmail.com>
2020-11-16 07:59:10 -0900

    samuel

--- tezbazar/website/views.py
+++ tezbazar/website/views.py
@@ -1,10 +1,31 @@
 from django.shortcuts import render
-marsoxod2 = "bartsucks"
+import os
+import time
+
+n = [1]
+
 def home(reguest):
-		man = open("textdottext.txt" , "r")
-		queen = man.read()
-		dormamu = queen
-		return render(reguest , 'home.html' , {'zaporoj' : dormamu})
+	if reguest.method == "POST" :
+		prname = reguest.POST['flint']
+		
+		with open("textdottext.txt" , "r+") as f :
+			fout = f.read()
+			mackintosh = fout.replace('Delivery infomation' , prname)
+			n[0] = n[0] + 1
+			hello = str(str(n[0]) + ".txt")
+			same = open( hello , "w+") 
+			jonatan = same.write(mackintosh)
+			same.close()
+			thisFile = hello
+			base = os.path.splitext(thisFile)[0]
+			os.rename(thisFile , base + ".html")
+				
+		return render(reguest , 'home.html' , {'zaporoj' : prname})	
+
+
+
+	else:
+		return render(reguest , 'home.html' , {})
 
 def blog(reguest1):
 	return render(reguest1, 'blog.html', {})
